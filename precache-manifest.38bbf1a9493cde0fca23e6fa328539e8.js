304089ab903960cf0b615e2f6ddf230ef4b2ff20
Mukesh Kumar Soni <ms153706@gmail.com>
2020-09-09 17:39:20 +0530

    Updates

---
+++ precache-manifest.38bbf1a9493cde0fca23e6fa328539e8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80c378202aaf7ef558e1461a4a49eaf",
+    "url": "/techhelp/index.html"
+  },
+  {
+    "revision": "09ea3b0ae12573808549",
+    "url": "/techhelp/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b6358723340bf2b5db9c",
+    "url": "/techhelp/static/css/main.9ba02cc4.chunk.css"
+  },
+  {
+    "revision": "09ea3b0ae12573808549",
+    "url": "/techhelp/static/js/2.d3c0172a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/techhelp/static/js/2.d3c0172a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6358723340bf2b5db9c",
+    "url": "/techhelp/static/js/main.025892e5.chunk.js"
+  },
+  {
+    "revision": "e2dae7831e8e9a898bce",
+    "url": "/techhelp/static/js/runtime-main.e061af27.js"
+  },
+  {
+    "revision": "21be860be9ad8097d3e067cb15ec7e5c",
+    "url": "/techhelp/static/media/android.21be860b.jfif"
+  },
+  {
+    "revision": "ffff3c9e54ee1db8bc6ce54ed45774f1",
+    "url": "/techhelp/static/media/app.ffff3c9e.jfif"
+  },
+  {
+    "revision": "bbb9f31c2690da329aa981cf4cb1b820",
+    "url": "/techhelp/static/media/digi_marketing.bbb9f31c.jfif"
+  },
+  {
+    "revision": "3b12e8ac548a77b9f6c02bed817ef757",
+    "url": "/techhelp/static/media/group.3b12e8ac.png"
+  },
+  {
+    "revision": "fadf9cc2477fb8601eb1b420e14c8186",
+    "url": "/techhelp/static/media/hosting.fadf9cc2.jfif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techhelp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6d7f57192866b5d27158fe1d5ff00e60",
+    "url": "/techhelp/static/media/sco.6d7f5719.jfif"
+  },
+  {
+    "revision": "74be33d758f60940261668d7b91de95a",
+    "url": "/techhelp/static/media/software.74be33d7.jfif"
+  },
+  {
+    "revision": "9ab07d8962a3005a0717ad05c4614754",
+    "url": "/techhelp/static/media/web.9ab07d89.jfif"
+  }
+]);
